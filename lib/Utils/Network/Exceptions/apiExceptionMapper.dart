f86c820a12c63e7b93c4b830c720f2d022f736ce
Gabija G <grgabija@gmail.com>
2020-12-23 22:12:21 +0200

    Created HTTP client and mapped network exceptions

---
+++ lib/Utils/Network/Exceptions/apiExceptionMapper.dart
@@ -0,0 +1,22 @@
+import 'package:cat_breed_app/Utils/Localization/strings.dart';
+import 'package:cat_breed_app/Utils/Network/Exceptions/apiException.dart';
+
+abstract class ApiExceptionMapper {
+  static String toErrorMessage(Object error) {
+    if (error is ApiException) {
+      if (error is ConnectionException) {
+        return Strings.connectionError;
+      } else if (error is ClientErrorException) {
+        return Strings.clientError;
+      } else if (error is ServerErrorException) {
+        return Strings.serverError;
+      } else if (error is EmptyResultException) {
+        return Strings.emptyResultError;
+      } else {
+        return Strings.unknownError;
+      }
+    } else {
+      return Strings.unknownError;
+    }
+  }
+}
